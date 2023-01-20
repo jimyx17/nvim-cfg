09b6d1c9341d197217317c29b952aafaa8eafeed
jimyx17 <jimyx17@gmail.com>
2023-01-20 21:12:14 +0100

    setup logger before loading the rest of plugins

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -3,7 +3,7 @@ local M = {}
 local fn = vim.fn
 local utils = require("utils")
 
-function download_plugin(url)
+local function download_plugin(url)
   -- Automatically install lazy
   local plugin_path = utils.get_plugin_image_dir()
   local _, plugin_name = utils.split_path(url)
@@ -26,19 +26,19 @@ function download_plugin(url)
   return plugin_install_path
 end
 
-function install_lazy()
+local function install_lazy()
   local lazy_install_path = download_plugin("https://github.com/folke/lazy.nvim")
---Setup Lazy path
+  --Setup Lazy path
   vim.opt.runtimepath:append(lazy_install_path)
 end
 
-function install_logger()
+local function install_logger()
   local logger_install_path = download_plugin("https://github.com/Tastyep/structlog.nvim")
---Setup logger path
+  --Setup logger path
   vim.opt.runtimepath:append(logger_install_path)
 end
 
-function init_lazy_cache()
+local function init_lazy_cache()
   local lazy_cache = require "lazy.core.cache"
   lazy_cache.setup {
     performance = {
@@ -48,17 +48,14 @@ function init_lazy_cache()
       },
     },
   }
-  --HACK: prevent lazy to call setup again
-  lazy_cache.setup = function() end
 end
 
 function M.boot()
   install_lazy()
   install_logger()
+  require("log"):set_level(vim.log.level)
   vim.opt.runtimepath:append(utils.join_paths(utils.get_plugin_image_dir(), "*"))
   init_lazy_cache()
-  require("log"):set_level(vim.log.level)
 end
-
 
 return M
